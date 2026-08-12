ec1f58bf6f0aaedd09804fa7eeef52cb0e7953af
8mykg <pikmin2936@gmail.com>
2026-08-13 05:17:20 +0900

    Extending PWA

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // 開発中(npm run dev)はキャッシュを無効化
+  disable: process.env.NODE_ENV === "development",
   workboxOptions: {
-    skipWaiting: true, // ★ workboxOptions の中に移動！
+    skipWaiting: true,
   },
 });
 
 const nextConfig: NextConfig = {
+  /* ここはスッキリ元通りでOK */
 };
+
 export default withPWA(nextConfig);
